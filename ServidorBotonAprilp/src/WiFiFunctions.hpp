116626a30780a20570fb383517610dd3bf505a63
= <gaston_15_cb@hotmail.com>
2021-09-18 19:39:18 -0300

    web terminada y servidor empezado

---
+++ ServidorBotonAprilp/src/WiFiFunctions.hpp
@@ -0,0 +1,11 @@
+/*
+ * Este archivo contiene las definiciones y  
+ * parámetros de la red WiFi  
+ * Modo de conexión : WiFi Direct 
+ * Puerto de comunicación: socket 18220 //(puerto sugerido )
+ * 
+*/
+
+#define PORT_TCP_SOCKET 18220 
+#define SSID_WIFI_DIRECT_NAME "*" 
+#define PASS_SSID_DIRECT_NAME "*"  
---
+++ ServidorBotonAprilp/src/main.cpp
@@ -0,0 +1,18 @@
+/*
+ *  Servidor para manejo remoto de juguetes 
+ *  Autor: Gaston Valdez 
+ *  Version 1.0 
+ *  Este software se utiliza en conjunto con el cliente 
+ *  que debe ponerse en el juguete/sistema a controlar 
+ * 
+*/
+
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
