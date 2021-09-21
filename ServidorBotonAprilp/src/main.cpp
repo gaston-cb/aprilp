50c448fcd7b86b51b9d859e886b573a94a3bd350
= <gaston_15_cb@hotmail.com>
2021-09-20 21:17:13 -0300

    version con reconocimiento de IP de clientes

--- ServidorBotonAprilp/src/main.cpp
+++ ServidorBotonAprilp/src/main.cpp
@@ -13,35 +13,39 @@
 #include "WiFiFunctions.hpp"
 #include <WebSocketsServer.h>
 #include "registroDelayWeb.h" 
-
+extern "C" {
+  #include "user_interface.h"
+}
 
 void mainPage()  ; 
 void configDelayClockClient() ; 
 void valorDelay() ; 
 void dataDelayWeb(uint8_t num, WStype_t type, uint8_t * payload, size_t length) ; 
-
+void newConnectClient(WiFiEventSoftAPModeStationConnected sta_info) ; 
+void obtainIPClients() ; 
 
 
 ESP8266WebServer server(80);    // puerto HTTP 
 WebSocketsServer webSocket = WebSocketsServer(81);  //PUERTO ESTANDAR WS 
-
-clientRegister senddataClientSockets{false ,0U }; 
+clientRegister senddataClientSockets{false ,0U , 0 }; 
 
 
 
 
 bool sendingDataClientSocket = false; 
 bool isDelayOn = false ;      //retraso de señal 
-
+bool waitDHCP = false  ; 
 
 
 
 void setup() {
+  static WiFiEventHandler e1; //dispatcher 
   Serial.begin(115200) ; 
   createWiFiNet();
   server.on("/", mainPage);
   server.on("/delay.html",  configDelayClockClient);
   server.on("/del", HTTP_POST , valorDelay);
+  e1 = WiFi.onSoftAPModeStationConnected(newConnectClient);
   webSocket.begin();
   webSocket.onEvent(dataDelayWeb);
   server.begin();
@@ -55,7 +59,15 @@ void loop() {
     sendingDataClientSocket = false;  
     Serial.print("tiempo delay: ") ; Serial.println(senddataClientSockets.timeDelay) ; 
     Serial.print(" isdelay: ") ; Serial.println(senddataClientSockets.isDelay) ; 
-  }   
+    //send data to client 
+  }
+  if (waitDHCP==true){
+    delay(2000) ; 
+    waitDHCP = false ; 
+    obtainIPClients() ; 
+    senddataClientSockets.numberClients = WiFi.softAPgetStationNum(); 
+//    Serial.println()
+  }
 }
 
 void mainPage() 
@@ -89,6 +101,7 @@ void valorDelay()
 
 void dataDelayWeb(uint8_t num, WStype_t type, uint8_t * payload, size_t length)
 {
+  //remote
   switch(type)
   {
     case WStype_DISCONNECTED:
@@ -111,3 +124,30 @@ void dataDelayWeb(uint8_t num, WStype_t type, uint8_t * payload, size_t length)
   } 
 }
     
+
+void newConnectClient(WiFiEventSoftAPModeStationConnected sta_info){
+  char last_mac[18]; 
+  sprintf(last_mac,"%02X:%02X:%02X:%02X:%02X:%02X", MAC2STR(sta_info.mac));
+  Serial.printf("MAC address : %s\n",last_mac);
+  Serial.printf("Id : %d\n", sta_info.aid);
+  waitDHCP = true ; 
+  
+} 
+
+
+void obtainIPClients(){
+  String cb ;
+  char last_mac[18];  
+  char* mac_device  = &last_mac[0] ; 
+  struct station_info *station_list = wifi_softap_get_station_info();
+  while (station_list != NULL) {
+    char station_mac[18] = {0}; sprintf(station_mac, "%02X:%02X:%02X:%02X:%02X:%02X", MAC2STR(station_list->bssid));
+    String station_ip = IPAddress((&station_list->ip)->addr).toString();
+    Serial.println("dentro de while") ; Serial.print("ip: ") ; 
+    Serial.println(station_ip) ; 
+    station_list = STAILQ_NEXT(station_list, next);
+  }
+  wifi_softap_free_station_info();
+  cb = "DHCP not ready or bad MAC address";
+  Serial.println(cb) ;  
+}
--- ServidorBotonAprilp/src/registroDelayWeb.h
+++ ServidorBotonAprilp/src/registroDelayWeb.h
@@ -3,5 +3,6 @@ struct clientRegister
 {
     bool isDelay ; 
     unsigned int timeDelay ; 
-    //char nameClient[] ; -- 
+    uint8_t numberClients;     
+    uint8_t Ipclients[4][4] ; // ip client 1  - 1- -1 -1  
 }; 
