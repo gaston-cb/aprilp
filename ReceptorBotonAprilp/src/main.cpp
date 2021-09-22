e227a31b9776fb4474b039189b495a637ea2fcae
= <gaston_15_cb@hotmail.com>
2021-09-21 23:59:39 -0300

    faltan OUIS espressif

--- ReceptorBotonAprilp/src/main.cpp
+++ ReceptorBotonAprilp/src/main.cpp
@@ -1,9 +1,29 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+void ConnectAP() ; 
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600) ; 
+  ConnectAP()  ; 
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+}
+
+void ConnectAP(){
+  const char *ssid = "APRILP" ; 
+  const char *psk  = "APRILPserver" ;  
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, psk);
+  Serial.print("Conectando a:\t");
+  Serial.println(ssid); 
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+    delay(200);
+   Serial.print('.');
+  }
+
+
 }
